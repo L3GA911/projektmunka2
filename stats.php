6d081dcbb8a98c7aa01d76e64b4c4c35187bed32
L3GA911 <43301827+L3GA911@users.noreply.github.com>
2021-05-04 21:03:46 +0200

    update

--- stats.php
+++ stats.php
@@ -5,11 +5,16 @@
 <div id="content" class="content">
     <span class="navname">Statisztikák</span><br>
 	<div class="content_container_stats">
-        <select id="cform" name="cform">
-            <option value="" selected disabled>Dolgozók</option>
-            <option value="1">Valaki Vagyok</option>
-            <option value="2">Mekk Elek</option>
-        </select><br>
+        <div class="dropdown">
+		  <button onclick="open_list()" class="dropbtn">Dolgozó kiválasztása</button>
+		  <div id="dropdown_list" class="dropdown-content">
+			<input type="text" placeholder="Keresés..." id="dropdown_search" onkeyup="filterFunction()">
+			<a href="">Lengyel Gábor</a>
+			<a href="">Ihász Viktor</a>
+			<a href="">Valaki Vagyok</a>
+		  </div>
+		</div><br>
+		<span>Dolgozó: XY</span>
 		<div id="chart_div"></div>
 	</div>
 </div>
